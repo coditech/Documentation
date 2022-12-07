d06107968208f0f75bc87f5481a45b0a3f5ac5cd
hasan <hasankanj2000@gmail.com>
2022-12-07 15:33:13 +0300

    step2

--- 03_Exercises/03_JavaScript/javascript-basics-01/step_2.js
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step_2.js
@@ -2,4 +2,4 @@ var name1 = 'Kanj'
 var surname = 'Hasan'
 var city = 'beirut'
 
-alert("name :" +name1 +"\n" + "surname :" + surname +  "\n" + "city :" +city)
+alert("name :" +name1 +"\n" + "surname :" + surname +  "\n" +  "city :" +city)
