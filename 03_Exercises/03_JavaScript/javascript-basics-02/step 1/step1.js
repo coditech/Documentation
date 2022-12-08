2c87eabbb40262c6862d25314f1d1b4976602445
hasan <hasankanj2000@gmail.com>
2022-12-08 23:40:29 +0300

    step5

--- 03_Exercises/03_JavaScript/javascript-basics-02/step 1/step1.js
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 1/step1.js
@@ -3,7 +3,3 @@ var image1 ;
 function mouseover() {
     image1 = document.querySelector('img').src ="./images/image1_2.jpg";
 }
-
-function mouseout() {
-    image1 = document.querySelector('img').src ="./images/image1.jpg";
-}
