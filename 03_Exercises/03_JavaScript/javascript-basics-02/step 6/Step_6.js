b494e58fa8bd02914b4570095e9da1ddcc1abd80
Mohannad <mohannadalrifai7@gmail.com>
2022-12-08 11:28:06 +0200

    Step6

--- 03_Exercises/03_JavaScript/javascript-basics-02/step 6/Step_6.js
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 6/Step_6.js
@@ -11,3 +11,10 @@ function test(){
     pic4.src="./images/image4_2.jpg"
     pic5.src="./images/image5_2.jpg"
 }
+function mouseOut(){
+    pic1.src="./images/image1.jpg"
+    pic2.src="./images/image2.jpg"
+    pic3.src="./images/image3.jpg"
+    pic4.src="./images/image4.jpg"
+    pic5.src="./images/image5.jpg"
+}
