3c62ed83919a1c15cb8bf0e36d3557e6f3627e1f
Rim-z <zeayterrim6@gmail.com>
2022-12-08 13:51:17 +0200

    step-8

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step6/step_6.js
@@ -0,0 +1,5 @@
+function divide() {
+  var num1 = document.getElementById("first_number").value;
+  var num2 = document.getElementById("second_number").value;
+  alert(num1 % num2);
+}
---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step7/step_7.js
@@ -0,0 +1,10 @@
+function myFunc(){
+  var x=document.getElementById("shoe_size").value
+var y=document.getElementById("year").value
+var w=  x *2;
+w=(w+5)*50;
+var result=(w-y)+1766;
+alert(result);
+
+
+}
