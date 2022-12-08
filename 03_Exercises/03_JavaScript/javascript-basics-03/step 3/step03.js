8d8110e5140eed0b9b0f8bee598f0f35a42693dd
hasan <hasankanj2000@gmail.com>
2022-12-09 01:55:28 +0300

    step3

---
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 3/step03.js
@@ -0,0 +1,10 @@
+var texts=document.getElementById('text');
+
+    function G() {
+    document.getElementById("text").style.color = "green";}
+    
+    function R() {  
+    document.getElementById("text").style.color = "red"; }
+
+    function B () {
+    document.getElementById("text").style.color = "blue"; }
