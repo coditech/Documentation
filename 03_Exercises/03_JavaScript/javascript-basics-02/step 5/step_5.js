80d7ea0523160a4cbcd53afd4e8ef41b394a7963
Mohannad <mohannadalrifai7@gmail.com>
2022-12-08 11:21:25 +0200

    step5

---
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 5/step_5.js
@@ -0,0 +1,16 @@
+let pic1 = document.querySelector("#image1");
+let pic2 = document.querySelector("#image2");
+let pic3 = document.querySelector("#image3");
+let pic4 = document.querySelector("#image4");
+let pic5 = document.querySelector("#image5");
+
+function test(){
+    pic1.src="./images/image1_2.jpg"
+    pic2.src="./images/image2_2.jpg"
+    pic3.src="./images/image3_2.jpg"
+    pic4.src="./images/image4_2.jpg"
+    pic5.src="./images/image5_2.jpg"
+
+
+
+}
