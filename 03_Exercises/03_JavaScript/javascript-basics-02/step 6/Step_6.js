e7d1929f8e2f6da9e749d97e848b6fa75a8e0b39
AhmadLAhmad <ahmadd.l.ahmadd1998@gmail.com>
2022-12-09 07:56:56 +0200

    finished step6

---
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 6/Step_6.js
@@ -0,0 +1,10 @@
+function mouseOver(){
+
+
+    document.querySelector(".first").style.marginLeft="40%";
+    }
+function mouseOut(){
+    
+    
+        document.querySelector(".first").style.margin="0px";
+        }
