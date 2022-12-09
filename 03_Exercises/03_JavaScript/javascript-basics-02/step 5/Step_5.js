090d4efb48f174ccb2d8f160209af7417f49e1ad
AhmadLAhmad <ahmadd.l.ahmadd1998@gmail.com>
2022-12-09 07:52:28 +0200

    finished step5

---
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 5/Step_5.js
@@ -0,0 +1,10 @@
+function mouseOver(){
+
+
+    document.querySelector(".container").style.marginLeft="40%";
+    }
+    function mouseOut(){
+    
+    
+        document.querySelector(".container").style.margin="0px";
+        }
