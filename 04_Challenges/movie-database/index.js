5616bfc6755cf134fc5954fbe52cb60ef054fdaf
Mohannad <mohannadalrifai7@gmail.com>
2023-01-06 15:11:08 +0200

    step4

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -20,3 +20,15 @@ app.get('/time', (req, res) => {
     const currentTime = new Date().toLocaleTimeString()
     res.json({status:200, message:currentTime})
   })
+  app.get('/hello/:id', (req, res) => {
+    const id = req.params.id
+    res.json({status:200, message:`Hello, ${id}`})
+  })
+  app.get('/search', (req, res) => {
+    const search = req.query.s
+    if (search) {
+      res.json({status:200, message:'ok', data:search})
+    } else {
+      res.status(500).json({status:500, error:true, message:'you have to provide a search'})
+    }
+  })
